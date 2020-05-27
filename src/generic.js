81f9d87d4a83e1d5be1a240644edc88cdbdb12da
Edward Wall <edward@edwardwall.me>
2020-05-27 03:51:38 +0100

    Move function to generic file

--- src/generic.js
+++ src/generic.js
@@ -1,4 +1,5 @@
-const HTTPS = require("https")
+const URL = require("url");
+const HTTPS = require("https");
 
 const INVALID_RESULT = {result:false};
 const VALID_RESULT   = {result:true};
@@ -123,10 +124,54 @@ function parsePolicy(policy) {
 }
 
 
+/**
+ * Parse Location header returned as HTTP redirect.
+ * @param {string} location
+ * @param {Object} options
+ * @returns {string}
+ */
+function parseLocation(location, options) {
+
+    let url;
+
+    // Check whether location is fully qualified URL
+    url = URL.parse(location);
+    if (url.protocol && url.hostname && url.path) {
+        return url.href;
+    }
+
+    // Check whether location is URL without protocol
+    if (location.startsWith("//")) {
+        url = URL.parse(options.protocol + location);
+
+        if (url.protocol && url.hostname && url.hostname.includes(".")) {
+            if (null === url.path) {
+                url.href += "/";
+            }
+            return url.href;
+        }
+    }
+
+    if (location.startsWith("/")) {
+        location = options.protocol + "//" + options.hostname + location;
+
+    } else {
+        location = options.protocol + "//" + options.hostname +
+            options.path.substring(0, options.path.lastIndexOf("/") + 1) +
+            location;
+    }
+
+    url = URL.parse(location);
+    return url.href;
+
+}
+
+
 module.exports = {
     INVALID_RESULT,
     VALID_RESULT,
     get,
     checkHeaderKeyValue,
-    parsePolicy
+    parsePolicy,
+    parseLocation
 };
