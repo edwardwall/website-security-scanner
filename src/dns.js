6b8612817717b2cb26400d27fbb02440d2fe2e5f
Edward Wall <edward@edwardwall.me>
2020-05-27 21:52:46 +0100

    Add check for CAA

---
+++ src/dns.js
@@ -0,0 +1,55 @@
+const GENERIC = require("./generic.js")
+
+
+/**
+ * Check DNS Certification Authority Authorization (CAA).
+ * @param {string} domain
+ * @param {CallbackResult} callback
+ * @returns {Result}
+ */
+async function caa(domain, callback) {
+
+    let wrappedCallback = (body) => {
+
+        body = JSON.parse(body);
+
+        if (!body.Answer) {
+            callback(GENERIC.INVALID_RESULT);
+            return;
+        }
+
+        let issuer = false;
+
+        for (elem of body.Answer) {
+
+            elem = elem.data;
+            elem = elem.substring("0 ".length);
+            elem = elem.toLowerCase().trim();
+
+            if (elem.startsWith("issue ") ||
+                elem.startsWith("issuewild ")) {
+
+                issuer = true;
+            }
+
+        }
+
+        if (issuer) {
+            callback(GENERIC.VALID_RESULT);
+        } else {
+            callback(GENERIC.INVALID_RESULT);
+        }
+
+    };
+
+    GENERIC.get(
+        "https://dns.google.com/resolve?type=CAA&dnssec=true&name=" + domain,
+        wrappedCallback
+    );
+
+}
+
+
+module.exports = {
+    caa
+}
