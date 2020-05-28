f9686ef5518ebdbd97ceb220c711f2780dac619b
Edward Wall <edward@edwardwall.me>
2020-05-28 20:21:14 +0100

    Move function

--- src/generic.js
+++ src/generic.js
@@ -137,54 +137,10 @@ function parsePolicy(policy) {
 }
 
 
-/**
- * Parse Location header returned as HTTP redirect.
- * @param {string} location
- * @param {Object} options
- * @returns {string}
- */
-function parseLocation(location, options) {
-
-    let url;
-
-    // Check whether location is fully qualified URL
-    url = URL.parse(location);
-    if (url.protocol && url.hostname && url.path) {
-        return url.href;
-    }
-
-    // Check whether location is URL without protocol
-    if (location.startsWith("//")) {
-        url = URL.parse(options.protocol + location);
-
-        if (url.protocol && url.hostname && url.hostname.includes(".")) {
-            if (null === url.path) {
-                url.href += "/";
-            }
-            return url.href;
-        }
-    }
-
-    if (location.startsWith("/")) {
-        location = options.protocol + "//" + options.hostname + location;
-
-    } else {
-        location = options.protocol + "//" + options.hostname +
-            options.path.substring(0, options.path.lastIndexOf("/") + 1) +
-            location;
-    }
-
-    url = URL.parse(location);
-    return url.href;
-
-}
-
-
 module.exports = {
     INVALID_RESULT,
     VALID_RESULT,
     get,
     checkHeaderKeyValue,
-    parsePolicy,
-    parseLocation
+    parsePolicy
 };
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 const NODE = {
-    URL: require("url")
+    URL: require("url"),
+    HTTP: require("http"),
+    HTTPS: require("https")
 };
 
 const TEST = {
@@ -56,19 +58,51 @@ class WebsiteSecurityScanner {
      */
     scan() {
 
-        Promise.all([
-            TEST.DNS.caa(this.domain, (result) => {
-                this.results.CAA = result;
-            }),
-            TEST.DNS.dnssec(this.domain, (result) => {
-                this.results.DNSSEC = result;
-            })
-        ]).then(() => {
-
-        });
-
     }
 
 }
 
 module.exports = WebsiteSecurityScanner;
+
+
+/**
+ * Parse Location header returned as HTTP redirect.
+ * @param {string} location
+ * @param {Object} options
+ * @returns {string}
+ */
+function parseLocation(location, options) {
+
+    let url;
+
+    // Check whether location is fully qualified URL
+    url = NODE.URL.parse(location);
+    if (url.protocol && url.hostname && url.path) {
+        return url.href;
+    }
+
+    // Check whether location is URL without protocol
+    if (location.startsWith("//")) {
+        url = NODE.URL.parse(options.protocol + location);
+
+        if (url.protocol && url.hostname && url.hostname.includes(".")) {
+            if (null === url.path) {
+                url.href += "/";
+            }
+            return url.href;
+        }
+    }
+
+    if (location.startsWith("/")) {
+        location = options.protocol + "//" + options.hostname + location;
+
+    } else {
+        location = options.protocol + "//" + options.hostname +
+            options.path.substring(0, options.path.lastIndexOf("/") + 1) +
+            location;
+    }
+
+    url = NODE.URL.parse(location);
+    return url.href;
+
+}
